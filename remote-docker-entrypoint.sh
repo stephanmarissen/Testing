5e8056126d4cc2d8cec9b933c3948f7240cc8622
Stephan <stephan.marissen@windesheim.nl>
2017-10-31 11:40:08 +1300

    Update remote-docker-entrypoint.sh

--- remote-docker-entrypoint.sh
+++ remote-docker-entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "Hello World!"
-echo "Hi mate!"
-#How long does it take for the change to go through?
+mysql -uroot -ppassword "USE agfirstdb1;"
+mysql -uroot -ppassword "DELETE FROM writers.sql WHERE id BETWEEN '1' AND '5000';"
