d9b6f1808b5d3b74871916e701813f427c2e6328
Stephan <stephan.marissen@windesheim.nl>
2017-11-01 11:53:48 +1300

    Update remote-docker-entrypoint.sh

--- remote-docker-entrypoint.sh
+++ remote-docker-entrypoint.sh
@@ -3,4 +3,4 @@ mysql -uroot -ppassword -e "CREATE DATABASE agfirstdb1;"
 mysql -uroot -ppassword -e "USE agfirstdb1;"
 mysql -uroot -ppassword agfirstdb1 < sqlfiles/writers.sql
 echo "table import complete"
-exit 0
+exit(0)
