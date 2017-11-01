89169b638f1a36838edff416a1ff6fceebee51df
Stephan <stephan.marissen@windesheim.nl>
2017-11-02 08:46:14 +1300

    Update remote-docker-entrypoint.sh

--- remote-docker-entrypoint.sh
+++ remote-docker-entrypoint.sh
@@ -1,4 +1,137 @@
 #!/bin/bash
+# Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+set -e
+
+echo "[Entrypoint] MySQL Docker Image 5.7.19-1.1.1"
+# Fetch value from server config
+# We use mysqld --verbose --help instead of my_print_defaults because the
+# latter only show values present in config files, and not server defaults
+_get_config() {
+	local conf="$1"; shift
+	"$@" --verbose --help 2>/dev/null | grep "^$conf" | awk '$1 == "'"$conf"'" { print $2; exit }'
+}
+
+# If command starts with an option, prepend mysqld
+# This allows users to add command-line options without
+# needing to specify the "mysqld" command
+if [ "${1:0:1}" = '-' ]; then
+	set -- mysqld "$@"
+fi
+
+if [ "$1" = 'mysqld' ]; then
+	# Test that the server can start. We redirect stdout to /dev/null so
+	# only the error messages are left.
+	result=0
+	output=$("$@" --verbose --help 2>&1 > /dev/null) || result=$?
+	if [ ! "$result" = "0" ]; then
+		echo >&2 '[Entrypoint] ERROR: Unable to start MySQL. Please check your configuration.'
+		echo >&2 "[Entrypoint] $output"
+		exit 1
+	fi
+
+	# Get config
+	DATADIR="$(_get_config 'datadir' "$@")"
+	SOCKET="$(_get_config 'socket' "$@")"
+
+	if [ -n "$MYSQL_LOG_CONSOLE" ] || [ -n "" ]; then
+		# Don't touch bind-mounted config files
+		if ! cat /proc/1/mounts | grep "etc/my.cnf"; then
+			sed -i 's/^log-error=/#&/' /etc/my.cnf
+		fi
+fi
+if [ ! -d "$DATADIR/mysql" ]; then
+if [ -f "$MYSQL_ROOT_PASSWORD" ]; then
+			MYSQL_ROOT_PASSWORD="$(cat $MYSQL_ROOT_PASSWORD)"
+			if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+				echo >&2 '[Entrypoint] Empty MYSQL_ROOT_PASSWORD file specified.'
+				exit 1
+			fi
+fi
+		mkdir -p "$DATADIR"
+		chown -R mysql:mysql "$DATADIR"
+
+		echo '[Entrypoint] Initializing database'
+		"$@" --initialize-insecure
+		echo '[Entrypoint] Database initialized'
+
+		"$@" --daemonize --skip-networking --socket="$SOCKET"
+
+		# To avoid using password on commandline, put it in a temporary file.
+		# The file is only populated when and if the root password is set.
+		PASSFILE=$(mktemp -u /var/lib/mysql-files/XXXXXXXXXX)
+		install /dev/null -m0600 -omysql -gmysql "$PASSFILE"
+		# Define the client command used throughout the script
+		# "SET @@SESSION.SQL_LOG_BIN=0;" is required for products like group replication to work properly
+		mysql=( mysql --defaults-extra-file="$PASSFILE" --protocol=socket -uroot -hlocalhost --socket="$SOCKET" --init-command="SET @@SESSION.SQL_LOG_BIN=0;")
+
+		if [ ! -z "" ];
+		then
+			for i in {30..0}; do
+				if mysqladmin --socket="$SOCKET" ping &>/dev/null; then
+					break
+				fi
+				echo '[Entrypoint] Waiting for server...'
+				sleep 1
+			done
+			if [ "$i" = 0 ]; then
+				echo >&2 '[Entrypoint] Timeout during MySQL init.'
+				exit 1
+			fi
+		fi
+
+mysql_tzinfo_to_sql /usr/share/zoneinfo | "${mysql[@]}" mysql
+
+if [ -z "$MYSQL_ROOT_HOST" ]; then
+			ROOTCREATE="ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+		else
+			ROOTCREATE="ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
+			CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
+			GRANT ALL ON *.* TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ; \
+			GRANT PROXY ON ''@'' TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;"
+fi
+
+
+"${mysql[@]}" <<-EOSQL
+			DELETE FROM mysql.user WHERE user NOT IN ('mysql.session', 'mysql.sys', 'root') OR host NOT IN ('localhost');
+			${ROOTCREATE}
+			FLUSH PRIVILEGES ;
+		EOSQL
+if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then
+			# Put the password into the temporary config file
+			cat >"$PASSFILE" <<EOF
+[client]
+password="${MYSQL_ROOT_PASSWORD}"
+EOF
+			#mysql+=( -p"${MYSQL_ROOT_PASSWORD}" )
+fi
+
 mysql -uroot -ppassword -e "CREATE DATABASE agfirstdb1;"
 mysql -uroot -ppassword -e "USE agfirstdb1;"
 mysql -uroot -ppassword agfirstdb1 < sqlfiles/writers.sql
+
+mysqladmin --defaults-extra-file="$PASSFILE" shutdown -uroot --socket="$SOCKET"
+		rm -f "$PASSFILE"
+		unset PASSFILE
+		echo "[Entrypoint] Server shut down"
+		echo '[Entrypoint] MySQL init process done. Ready for start up.'
+	fi
+
+	echo "[Entrypoint] Starting MySQL 5.7.19-1.1.1"
+fi
+
+echo -n "[Entrypoint] Container IP Address is: "; cat /etc/hosts | tail -1
+
+exec "$@"
