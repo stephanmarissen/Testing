97705b106ae2be78a7f08d0ba2cbe2016e995f2a
Stephan <stephan.marissen@windesheim.nl>
2017-11-27 16:02:57 +1300

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,3 +1,3 @@
 mysql -uroot -ppassword -e "CREATE DATABASE agfirstdb1;"
 mysql -uroot -ppassword -e "USE agfirstdb1;"
-mysql -uroot -ppassword agfirstdb1 < sqlfiles/writers.sql
+mysql -uroot -ppassword agfirstdb1 < importfiles/writers.sql
